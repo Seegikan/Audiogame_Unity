271b799a4b4a95da2a9ea396f1c7b8cf7600398b
Alexander Iñiguez <62976082+Seegikan@users.noreply.github.com>
2023-11-03 12:50:07 -0600

    no se que es pero bueno

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,17 +12,15 @@ public class Movement : MonoBehaviour
     [SerializeField] private Rigidbody rigidBody;
     private Vector3 dirMovement;
     [SerializeField] private CapsuleCollider collider;
+
+    //Ejemplo de UnityEvent
     [SerializeField] UnityEvent _OnWalking;
 
 
     void Update()
     {
-       // transform.rotation = Quaternion.Euler(Vector3.right * 0.1f); 
         _OnWalking?.Invoke();
-        //dirMovement = new Vector3(Input.GetAxisRaw("Horizontal") * speedMovement, 0f, Input.GetAxisRaw("Vertical") * speedMovement);
         dirMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized * speedMovement;
-        //dirMovement = cameraV.transform.TransformDirection(dirMovement);
-       // dirMovement = Vector3.ProjectOnPlane(dirMovement, Vector3.up);
         dirMovement.y = rigidBody.velocity.y;
         rigidBody.velocity = dirMovement;
     }
